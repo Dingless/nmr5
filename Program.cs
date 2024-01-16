a0ac445421dbd0ef1b8181726994ca7284dd0033
Dingless <mohameddeg.abdulahi@elev.ga.ntig.se>
2024-01-16 16:07:02 +0100

    first commit

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿string  lösenord = "nopass";
+string rättlösenord = "";
+while (lösenord != "rättlösenord")
+{
+    Console.WriteLine("Ange ditt lösenord");
+    lösenord = Console.ReadLine();
+    if (rättlösenord != "lösenord")
+    {
+        Console.WriteLine("Fel lösenord försök igen");
+    }
+}
